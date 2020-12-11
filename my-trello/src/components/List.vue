b3ace0fa4a16f5d315e6e7138d47f10a2d698eaf
inui <inui@spookies.co.jp>
2020-12-11 12:16:52 +0900

    サイトが表示されないー

--- my-trello/src/components/List.vue
+++ my-trello/src/components/List.vue
@@ -9,6 +9,22 @@
 
 <script>
 export default {
-    
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    listIndex: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    removeList: function() {
+      if(confirm('本当にこのリストを削除しますか？')){
+        this.$store.dispatch('removelist', { listIndex: this.listIndex })
+      }
+    },
+  }
 }
 </script>
